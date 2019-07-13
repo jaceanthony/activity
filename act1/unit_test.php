96365043c6b7e76f6fd73e82ed14af66452e9f29
CC105@ccs.local <CC105@ccs.local>
2019-07-14 03:43:54 +0800

    first commit activity 1

---
+++ act1/unit_test.php
@@ -0,0 +1,37 @@
+<?php
+require("Database.php");
+$dbObj = new Database();
+//case1
+// $result= $dbObj->execute("INSERT INTO tbluser(name,age,address,nationality) VALUES
+// ('Jace',20,'lahug','Filipino')");
+//echo "Case1 Result: ".$result;
+
+//case2
+//  $result = $dbObj->execute("UPDATE tbluser SET name ='Jace Anthony' WHERE id =15");
+//  echo "Case2 Result: ".$result;
+
+//case3
+//  $result = $dbObj->execute("SELECT * FROM tbluser");
+// echo "Case3 Result: ".$result;
+
+//case4
+//   $result = $dbObj->execute("DELETE FROM tbluser WHERE id=1");
+//  echo "Case4 Result: ".$result;
+
+//case5
+// $result= $dbObj->execute("INSER INTO tbluser(name,age,address,nationality) VALUES
+// ('Jace',20,'lahug','Filipino')");
+// echo "Case5 Result: ".$result;
+
+//case6
+// $result = $dbObj->execute("UPDAE tbluser SET name ='Jace Anthony' WHERE id =15");
+//  echo "Case6 Result: ".$result;
+
+//case7 
+//  $result = $dbObj->execute("SELEC * FROM tbluser");
+//  echo "Case7 Result: ".$result;
+
+//case8
+//  $result = $dbObj->execute("DELTE FROM tbluser WHERE id=1");
+//  echo "Case8 Result: ".$result;
+?>
